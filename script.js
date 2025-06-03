c8eb2be9baa987924d1a264a73316fb41164641d
habibur-rahman-tanvir <habibur353637@gmail.com>
2025-06-03 21:14:00 +0600

    Completed calculator project
    -For now I complete my calculator app project. But I know there have lot of bug. I will fix everything later

--- script.js
+++ script.js
@@ -6,11 +6,119 @@ let previewField = document.querySelector("#preview");
 let keypad = document.querySelector("#keypad");
 
 let input = "";
+let preview = "";
 
 keypad.addEventListener("click", function (event) {
     let button = event.target.closest("button");
-
     if (!button || !keypad.contains(button)) return;
-
-    console.log(button.value);
+    let type = button.dataset.type;
+    if (type == "ac") {
+        clearAll();
+        return;
+    }
+    if (type == "number") {
+        addDigit(button.value);
+        calculate(input);
+        return;
+    }
+    if (type == "operator") {
+        addOperator(button.value);
+        calculate(input);
+        return;
+    }
+    if (type == "point") {
+        addPoint(button.value);
+        calculate(input);
+        return;
+    }
+    if (type == "delete") {
+        removeDigit();
+        calculate(input);
+        return;
+    }
+    if (type == "equal") {
+        input = "" + preview;
+        preview = "";
+        inputField.innerHTML = input;
+        previewField.innerHTML = preview;
+        return;
+    }
 });
+
+function addDigit(digit) {
+    let lastNumber = getLastNumber(input);
+    if (digit != "0") {
+        if (lastNumber == "0" && !lastNumber.includes(".")) {
+            input = input.slice(0, input.length - 1) + digit
+        } else {
+            input += digit;
+        }
+        return;
+    }
+    if (!lastNumber || lastNumber.includes(".") || lastNumber != 0) {
+        input += digit;
+        return;
+    }
+}
+
+function removeDigit() {
+    if (input.length === 0) return;
+    input = input.slice(0, input.length - 1);
+}
+
+function addPoint(point) {
+    let lastNumber = getLastNumber(input);
+    if (!lastNumber) {
+        input += "0" + point;
+        return;
+    }
+    if (!lastNumber.includes(".")) {
+        input += point;
+        return;
+    }
+}
+
+function clearAll() {
+    input = "";
+    preview = "";
+    inputField.innerHTML = "";
+    previewField.innerHTML = "";
+}
+
+function addOperator(operator) {
+    if (input.length == 0 && (operator == "*" || operator == "/")) return;
+    if (isFinite(input.at(-1)) || input.length == 0) {
+        input += operator;
+    } else {
+        input = input.slice(0, input.length - 1) + operator;
+    }
+}
+
+function getLastNumber(equation) {
+    function lastOperatorIndex() {
+        return Math.max(
+            equation.lastIndexOf("+"),
+            equation.lastIndexOf("-"),
+            equation.lastIndexOf("*"),
+            equation.lastIndexOf("/")
+        );
+    }
+    return equation.slice(lastOperatorIndex() + 1);
+}
+
+function calculate(equation) {
+    if (isFinite(input.at(-1))) {
+        preview = eval(equation) ?? "";
+    } else {
+        preview = eval(input.slice(0, input.length - 1)) ?? "";
+    }
+    let equationAsStr = equation.replaceAll("*", "×");
+    equationAsStr = equationAsStr.replaceAll("/", "÷");
+    inputField.innerHTML = equationAsStr;
+    if (isFinite(preview)) {
+        previewField.innerHTML = preview;
+    } else {
+        preview = ""
+        previewField.innerHTML = "Math ERROR";
+    }
+}
