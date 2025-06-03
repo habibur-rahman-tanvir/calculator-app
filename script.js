d2ba4d182e13f07aa8abdc301ac6e7e31d2fdf69
habibur-rahman-tanvir <habibur353637@gmail.com>
2025-06-03 16:02:56 +0600

    Added button listener
    -Added button listener using event delegation add get button value

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("Script Loaded");
+
+let inputField = document.querySelector("#input");
+let previewField = document.querySelector("#preview");
+let keypad = document.querySelector("#keypad");
+
+let input = "";
+
+keypad.addEventListener("click", function (event) {
+    let button = event.target.closest("button");
+
+    if (!button || !keypad.contains(button)) return;
+
+    console.log(button.value);
+});
